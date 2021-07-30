cee8e7764edb30bf74662ec85213269ef12f9654
AleksandrBoguckiy <vagula91@gmail.com>
2021-07-30 14:27:50 +0700

    SN (git-page)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import {Header} from './components/Header/Header';
 import {Profile} from './components/Profile/Profile';
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, HashRouter, Route} from 'react-router-dom'
 import {News} from './components/News/News';
 import {Music} from './components/Music/Music';
 import {Settings} from './components/Settings/Settings';
@@ -12,7 +12,7 @@ import UsersContainer from "./components/Users/UsersContainer";
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <NavbarContainer />
@@ -25,7 +25,7 @@ const App = () => {
                     <Route path='/settings' component={Settings}/>
                 </div>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
